44346ce09a6639fce6f49b8d322ed72795a38a2c
Zainab Lawal <zainablaw1012@gmail.com>
2021-11-11 19:46:50 +0300

    configured postgresql

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,3 +136,5 @@ LOGIN_REDIRECT_URL='/afterlogin'
 if 'DATABASE_URL' in os.environ:
     import dj_database_url
     DATABASES = {'default': dj_database_url.config()}
+
+django_heroku.settings(locals())    
