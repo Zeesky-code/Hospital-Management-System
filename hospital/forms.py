9184121707debdfb34697ce687ba7c22ab337c2c
Zainab Lawal <zainablaw1012@gmail.com>
2021-11-11 19:29:35 +0300

    migrated

--- hospital/forms.py
+++ hospital/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from . import models
-
+from django.contrib.auth import (
+    authenticate, get_user_model, password_validation,
+)
 
 
 #for admin signup
